f0bbf6608ce4b5136a0b80a17ababebdba69f509
Nan Wang <nwang23@ucsc.edu>
2019-03-08 17:49:07 -0800

    model modified+weight adjusted

--- Nan's work/run.m
+++ Nan's work/run.m
@@ -9,26 +9,38 @@ global T;
 global Xtarg;
 global w;
 global v;
-
+global l;
 v = 1;
 T = 10;
-X0 = [0, 2.5]';
-w = [1, 1];
-Xtarg = [10, 7.5]';
+
+
+
+X0 = [0, 2.5, 0]';
+Xtarg = [10, 7.5, 0]';
 
 A = [];
 b = [];
+Aeq = [];
+beq = [];
+lb = [];
+ub = [];
 x0 = rand(1, N);
+%options = optimoptions('fmincon','Display','iter','Algorithm','sqp');
 u = fmincon(@cost_fun,x0,A,b);
 
-X = zeros(2, N + 1);
+X = zeros(3, N + 1);
 X(:,1) = X0;
 deltaT = T/N;
 
 for i = 1:N
-    X(:,i+1) = [v*cos(u(i))*deltaT; v*sin(u(i))*deltaT]+X(:,i);
+    X(:,i+1) = [v*cos(X(3,i))*deltaT; v*sin(X(3,i))*deltaT; v*deltaT*tan(u(i))/l]+X(:,i);
 end
 
 figure(1)
-plot(X(1,:),X(2,:),'-')
+plot([X0(1) X(1,:) Xtarg(1)],[X0(2) X(2,:) Xtarg(2)],'-');
+hold on
+plot(X0(1),X0(2),'r*');
+plot(Xtarg(1),Xtarg(2),'b*');
+xlabel('x (m)');
+ylabel('y (m)');
 grid on
