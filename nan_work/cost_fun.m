f877f399a5ac3b49eb24848fe347e18949f215f2
Nan Wang <nwang23@ucsc.edu>
2019-03-10 19:10:45 -0700

    Add moving obstacles and safety component in obj function

--- nan_work/cost_fun.m
+++ nan_work/cost_fun.m
@@ -2,43 +2,66 @@ function cost = cost_fun(u)
 %FUN Summary of this function goes here
 %   Detailed explanation goes here
 global X0;
-global T;
 global Xtarg;
-global w;
+global X;
 global v;
+
+global w1;
+global w2;
+global w3;
+global w4;
+
+global Xobs;
+global Xobs0;
+global vobs;
+
+global T;
 global l;
-
-w1 = .5;
-w2 = [1, 5, 10];
-w3 = 1;
-w = [w1, w2, w3];
-
+global TTC;
 
 N = length(u);
 deltaT = T/N;
 X = zeros(3, N + 1);
 X(:,1) = X0;
-l = 2.65;
 
 for i = 1:N
     X(:,i+1) = [v*cos(X(3,i))*deltaT; v*sin(X(3,i))*deltaT; v*deltaT*tan(u(i))/l]+X(:,i);
 end
 
 %% cost1: efficiency(length)
-vector1 = diff(X')';
+vector1 = diff([X Xtarg]')';
 vector2 = sqrt(vector1(1,:).^2+vector1(2,:).^2);
 cost1 = sum(vector2);
 
 
-%% cost2: (distance to target state)
+%% cost2: last state distance (distance to target state)
 cost2 = X(:,N+1)-Xtarg;
 
-%% cost3: (jerk)
+%% cost3: smoothness (jerk)
 dX = diff(X')'/deltaT;
 ddX = diff(dX')'/deltaT;
 dddX = diff(ddX')'/deltaT;
 cost3 = max(sqrt(dddX(2,:).^2)+sqrt(dddX(1,:).^2));
 
-cost = sum([cost1*w1 norm(cost2'.*w2) cost3*w3]);
+%% cost4: safety
+Xobs = zeros(3,N+1);
+Xobs(:,1) = Xobs0;
+TTC = zeros(1,N);
+for i = 1:N
+    Xobs(:,i+1) = [vobs*cos(Xobs(3,i))*deltaT; vobs*sin(Xobs(3,i))*deltaT; vobs*deltaT*tan(0)/l]+Xobs(:,i);
+end
+
+vector1 = Xobs - X;
+for i =1:N
+    v1 = [v*cos(X(3,i)), v*sin(X(3,i))]*vector1(1:2,i)/norm(vector1(1:2,i));
+    v2 = [vobs*cos(Xobs(3,i)), vobs*sin(Xobs(3,i))]*vector1(1:2,i)/norm(vector1(1:2,i));
+    TTC(i) = min(norm(vector1(1:2,i))/(v1-v2),100);
+    if (TTC(i)<0)
+        TTC(i) = 100;
+    end
+ end
+cost4 = min(TTC);
+
+cost = sum([cost1*w1 norm(cost2'.*w2) cost3*w3 cost4*w4]);
 end
 
--- nan_work/run.m
+++ nan_work/run.m
@@ -2,45 +2,72 @@ clc
 clear all
 close all
 
-N = 100;
-
 global X0;
-global T;
 global Xtarg;
-global w;
+global X;
 global v;
+
+global w1;
+global w2;
+global w3;
+global w4;
+
+global Xobs;
+global Xobs0;
+global vobs;
+
+global T;
 global l;
-v = 1;
-T = 10;
-
-
-
+global TTC;
+%% ego-vehicle state
+l = 2.65;
 X0 = [0, 2.5, 0]';
 Xtarg = [10, 7.5, 0]';
+v = 1;
 
+%% guest-vehicle state
+Xobs0 = [10; 2.5; 0];
+vobs = 0.5;
+
+%% weight of each component in objective function
+w1 = .5;
+w2 = [1, 5, 10];
+w3 = 1;
+w4 = -0.5;
+
+%% Receding Time Horizon setting
+T = 10;
+N = 100;
+
+%% main
 A = [];
 b = [];
 Aeq = [];
 beq = [];
-lb = [];
-ub = [];
+lb = -0.4922 * ones(1, N);
+ub = 0.4922 * ones(1, N);
 x0 = rand(1, N);
-%options = optimoptions('fmincon','Display','iter','Algorithm','sqp');
 u = fmincon(@cost_fun,x0,A,b);
 
-X = zeros(3, N + 1);
-X(:,1) = X0;
-deltaT = T/N;
+% X = zeros(3, N + 1);
+% X(:,1) = X0;
+% deltaT = T/N;
 
-for i = 1:N
-    X(:,i+1) = [v*cos(X(3,i))*deltaT; v*sin(X(3,i))*deltaT; v*deltaT*tan(u(i))/l]+X(:,i);
-end
+% for i = 1:N
+%     X(:,i+1) = [v*cos(X(3,i))*deltaT; v*sin(X(3,i))*deltaT; v*deltaT*tan(u(i))/l]+X(:,i);
+% end
 
 figure(1)
-plot([X0(1) X(1,:) Xtarg(1)],[X0(2) X(2,:) Xtarg(2)],'-');
+plot([X0(1) X(1,:) Xtarg(1)],[X0(2) X(2,:) Xtarg(2)],'r-');
+hold on
+plot(Xobs(1,:),Xobs(2,:),'b-');
 hold on
 plot(X0(1),X0(2),'r*');
 plot(Xtarg(1),Xtarg(2),'b*');
 xlabel('x (m)');
 ylabel('y (m)');
+grid on
+
+figure(2)
+plot(TTC);
 grid on
